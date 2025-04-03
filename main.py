ed3ea75d796251c698573deffd0f950de407a57f
Waseem Alnalouti <wasimalnaloti@gmail.com>
2025-04-03 14:41:03 +0200

    upload hangman file

---
+++ main.py
@@ -0,0 +1,9 @@
+# main.py
+from utils.game import Hangman
+
+if __name__ == "__main__":
+    # Create a new Hangman game instance
+    game = Hangman()
+    
+    # Start the game
+    game.start_game()
